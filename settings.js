96b3db26c6b4281cfd07a19e2c6acc8672354e34
DDaswE <129628235+DDaswE@users.noreply.github.com>
2026-04-03 12:08:53 -0400

    Add project files

---
+++ settings.js
@@ -0,0 +1,13 @@
+(function () {
+  const DEFAULT_SETTINGS = {
+    enabled: true,
+    keepRecentTurns: 24,
+    revealBatchSize: 8,
+    useVisibilityBoost: true,
+    compactActionBars: true
+  };
+
+  window.OGC_SHARED = {
+    DEFAULT_SETTINGS
+  };
+})();
